5bba8363b72c0cfc34e54646f58b9fccb4bcba97
jake <loncerasjakerei@gmail.com>
2022-05-05 18:53:19 +0800

    Added model validation for article title and body

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,19 @@
+# frozen_string_literal: true
+
 class Article < ApplicationRecord
-  validates :body, presence: false, length: { maximum: 20, minimum: 3 }, uniqueness: false
+  validates :body,
+            presence: true,
+            length: {
+              maximum: 140,
+              minimum: 3
+            },
+            uniqueness: false
+
+  validates :title,
+            presence: true,
+            length: {
+              maximum: 20,
+              minimum: 3
+            },
+            uniqueness: true
 end
