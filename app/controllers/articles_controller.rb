f8a1e4efe2550c99ffd412472bdbca1869c46a3b
jake <loncerasjakerei@gmail.com>
2022-05-05 19:19:15 +0800

    Added controller action create and view for new form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,4 +13,26 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :uprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params
+      .require(:article)
+      .permit(
+        :title,
+        :body,
+        :image
+      )
+  end
 end
